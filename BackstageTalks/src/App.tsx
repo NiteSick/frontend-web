f92accd573c009bdb1b90b10eb46f38407d85c19
Deepanshu Jangid <deepanshu21270@gmail.com>
2024-06-22 14:04:48 +0530

    backtalk project
    scroll snapping created

---
+++ BackstageTalks/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="wrapper">
+        <div className="container" id="one"></div>
+        <div className="container" id="two"></div>
+        <div className="container" id="three"></div>
+        <div className="container" id="four"></div>
+        <div className="container" id="five"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
